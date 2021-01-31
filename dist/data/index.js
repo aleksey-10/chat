45c1f9b9bc4f5b165636de20a2e93dca08e0e2e9
aleksey-10 <alexcare1337@gmail.com>
2021-01-31 10:49:13 +0200

    doesn't work

---
+++ dist/data/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.messages = void 0;
+exports.messages = [];
+//# sourceMappingURL=index.js.map
---
+++ dist/index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const index_1 = require("./data/index");
+const PORT = process.env.PORT || 5000;
+const app = express_1.default();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: { origin: '*' },
+});
+app.use(express_1.default.json());
+app.use(cors_1.default());
+//app.use(express.static('client/dist'));
+//app.get('/', (req, res) => {
+//  res.sendFile('client/dist/index.html');
+//});
+app.get('/api/chat', (req, res) => {
+    res.json({ messages: index_1.messages });
+});
+io.on('connection', (socket) => {
+    socket.on('message', ({ userName, text }) => {
+        const message = { id: Date.now(), userName, text };
+        index_1.messages.push(message);
+        io.emit('message', message);
+    });
+});
+try {
+    http.listen(PORT, () => {
+        console.log(`Server has been started on port ${PORT}...`);
+    });
+}
+catch (e) {
+    console.log('Server error', e);
+    process.exit(1);
+}
+//# sourceMappingURL=index.js.map
